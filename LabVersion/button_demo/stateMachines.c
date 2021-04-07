10ba285a9d1d5838ec0b45f493d882a7b4b1dcaf
justus <jfrausto10@gmail.com>
2021-04-07 12:37:38 -0600

    made a project compliant program ---- Not final

--- LabVersion/button_demo/stateMachines.c
+++ LabVersion/button_demo/stateMachines.c
@@ -1,24 +1,34 @@
 #include "led.h"
 #include "switches.h"
 
-void switchSM(){
-  if(switch_state_down1) red_on ^= 1;
-  if(switch_state_down2) green_on ^= 1;
-  /*
-  if(switch_state_down3 && !switch_state_down1) buzzer_set_period(2000);
-  else if (switch_state_down3 && switch_state_down1) buzzer_set_period(1000);
+void switchSM(){  
+  if(switch_state_down1 && switch_state_down2){
+    red_on ^= 1;
+    if(red_on == 1) buzzer_set_period(4000);
+    else buzzer_set_period(0);
+  }
+  
+  else if (switch_state_down3){
+    green_on ^= 1;
+    if(green_on == 1) buzzer_set_period(2000);
+    else buzzer_set_period(0);
+  }
+  else if (switch_state_down4) stateMachine1();
+  
   else buzzer_set_period(0);
-  */
+
+  
+  
   led_changed = 1;
   led_update();
 }
 
-/*
+
 void stateMachine1(){
   static char state = 0;
   switch(state){
-  case 0: red_on = 1; state++; break;
-  case 1: green_on = 1; state++; break;
+  case 0: red_on = 1; green_on = 0; state = 1; break;
+  case 1: green_on = 1; red_on = 0; state = 2; break;
   default: state = 0;
   }
-*/
+}
